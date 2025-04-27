85ecc93510a1cdafff43697e679b0b6f65052d7b
Iqbal <aodihis@gmail.com>
2025-04-27 09:56:39 +0700

    * Add validator for playlist and user.

---
+++ src/validators/users/index.js
@@ -0,0 +1,25 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const {AuthenticationPostPayloadSchema, AuthenticationPutPayloadSchema, AuthenticationDeletePayloadSchema} = require('./schema');
+
+const AuthenticationValidator = {
+    validateAuthenticationPostPayload: (payload) => {
+        const validationResult = AuthenticationPostPayloadSchema.validate(payload);
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+    validateAuthenticationPuttPayload: (payload) => {
+        const validationResult = AuthenticationPutPayloadSchema.validate(payload);
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+    validateAuthenticationDeletePayload: (payload) => {
+        const validationResult = AuthenticationDeletePayloadSchema.validate(payload);
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+};
+
+module.exports = AuthenticationValidator;
---
+++ src/validators/users/schema.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const AuthenticationPostPayloadSchema = Joi.object({
+    username : Joi.string().required(),
+    password : Joi.string().required(),
+});
+
+const AuthenticationPutPayloadSchema = Joi.object({
+    refreshToken : Joi.string().required(),
+});
+
+const AuthenticationDeletePayloadSchema = Joi.object({
+    refreshToken : Joi.string().required(),
+});
+
+module.exports = {AuthenticationPostPayloadSchema, AuthenticationPutPayloadSchema, AuthenticationDeletePayloadSchema};
